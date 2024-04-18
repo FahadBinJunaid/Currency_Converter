7d3e1607c0d188d5c854fae054c9482d622c2446
FahadBinJunaid <160081833+FahadBinJunaid@users.noreply.github.com>
2024-04-18 14:44:08 -0700

    Currency_Converter
    This is a simple Currency_Converter Coding

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const Currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+    KWD: 0.31,
+    CAD: 1.36
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "Enter From Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'KWD', 'CAD']
+    },
+    {
+        name: 'to',
+        message: "Enter To Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'KWD', 'CAD']
+    },
+    {
+        name: 'amount',
+        message: "Enter your amount",
+        type: 'number',
+    }
+]);
+let fromAmount = Currency[user_answer.from];
+let toAmount = Currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convetedAmount = baseAmount * toAmount;
+console.log(convetedAmount);
